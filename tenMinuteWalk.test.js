7c258eaf7b1ffda83b0c5b4f934c75494d33426e
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-03-29 15:05:41 +0100

    Tests refactored for clarity

--- tenMinuteWalk.test.js
+++ tenMinuteWalk.test.js
@@ -2,23 +2,27 @@ const tenMinuteWalk = require('./tenMinuteWalk');
 
 describe('tenMinuteWalk', () => {
   it('outputs false if walk is less than ten minutes long', () => {
-    expect(tenMinuteWalk(['w', 's'])).toBe(false);
-  });
-
-  it('outputs true if walk is exactly ten minutes long', () => {
-    expect(tenMinuteWalk(['w', 'w', 'w', 'w', 'w', 'e', 'e', 'e', 'e', 'e'])).toBe(true);
+    expect(tenMinuteWalk(['w', 'w'])).toBe(false);
   });
 
   it('outputs false if walk is more than ten minutes long', () => {
-    expect(tenMinuteWalk(['w', 'w', 'w', 'w', 'w', 'w', 's', 's', 's', 's', 's', 's'])).toBe(false);
+    expect(tenMinuteWalk(['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'])).toBe(false);
   });
 
-  it('outputs true if walk is exactly ten minutes long and w & e directions balance', () => {
+  it('outputs true if walk is exactly ten minutes long and  w & e directions balance', () => {
     expect(tenMinuteWalk(['w', 'w', 'w', 'w', 'w', 'e', 'e', 'e', 'e', 'e'])).toBe(true);
+  });
+
+  it('outputs false if walk is exactly ten minutes long and  w & e directions do not balance', () => {
+    expect(tenMinuteWalk(['w', 'w', 'w', 'w', 'w', 'w', 'w', 'e', 'e', 'e'])).toBe(false);
   });
 
   it('outputs true if walk is exactly ten minutes long and s & n directions balance', () => {
     expect(tenMinuteWalk(['n', 'n', 'n', 'n', 'n', 's', 's', 's', 's', 's'])).toBe(true);
+  });
+
+  it('outputs false if walk is exactly ten minutes long and s & n directions do not balance', () => {
+    expect(tenMinuteWalk(['n', 'n', 'n', 'n', 'n', 'n', 'n', 's', 's', 's'])).toBe(false);
   });
 });
 
