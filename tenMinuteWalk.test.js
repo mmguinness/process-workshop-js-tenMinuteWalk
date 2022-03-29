67dd86e4dbcc5efbe94f94e1a810da3439d21205
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-03-29 12:34:07 +0100

    Test passing for more than 10 minute length of walk

--- tenMinuteWalk.test.js
+++ tenMinuteWalk.test.js
@@ -1,8 +1,16 @@
 const tenMinuteWalk = require('./tenMinuteWalk');
 
 describe('tenMinuteWalk', () => {
-  it('outputs false if walk is less than ten minutes', () => {
+  it('outputs false if walk is less than ten minutes long', () => {
     expect(tenMinuteWalk(['w', 's'])).toBe(false);
+  });
+
+  it('outputs true if walk is exactly ten minutes long', () => {
+    expect(tenMinuteWalk(['w', 'w', 'w', 'w', 'w', 's', 's', 's', 's', 's'])).toBe(true);
+  });
+
+  it('outputs false if walk is more than ten minutes long', () => {
+    expect(tenMinuteWalk(['w', 'w', 'w', 'w', 'w', 'w', 's', 's', 's', 's', 's', 's'])).toBe(false);
   });
 });
 
